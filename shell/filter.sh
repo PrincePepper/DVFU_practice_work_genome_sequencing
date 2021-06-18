f6d53a76e256d61dbf9187fa71c824625617c89d
AlexPishchikov <cocacolaspriteman@gmail.com>
2021-06-18 13:16:06 +1000

    оптимизация: замена вызова cut на вызов tr

--- shell/filter.sh
+++ shell/filter.sh
@@ -1,11 +1,13 @@
-clear;
 gzip -cd $1 > data_file;
-grep -A1 --no-group-separator "@HWI-" data_file > filtered_data_temp;
-cut -d: -f '4 5' filtered_data > cut_data_temp;
+grep -A1 --no-group-separator "@HWI-" data_file > filtered_data;
+
+cat filtered_data | tr "#" ":" | cut -d: -f '4 5' > cut_data;
+
+# cut -d: -f '4 5' filtered_data > cut_data_temp;
 
 # cut -d# -f1 cut_data_temp | tr "N" " " > cut_data;
 
-cut -d# -f1 cut_data_temp > cut_data;
+# cut -d# -f1 cut_data_temp > cut_data;
 
 awk -FN -f cut.awk cut_data > clear_data;
 
@@ -14,8 +16,6 @@ awk -FN -f cut.awk cut_data > clear_data;
 # awk -f '{NR % 2 == 0 print cut filtered_data -d: -f "4 5" > cut_data else }'
 
 rm data_file;
-rm filtered_data_temp;
-rm cut_data_temp;
 rm filtered_data;
 
 # sed 'N;s/\n/ - /' new_R39-L6-READ2-Sequences.txt > temp_output.txt 
