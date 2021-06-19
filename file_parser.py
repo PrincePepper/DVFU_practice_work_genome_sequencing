57da6b54d22bd2e9cafcea612929b0a64d7700e7
Stanislav Nagornov <58802975+KA80@users.noreply.github.com>
2021-06-20 01:02:06 +1000

    Upd: algorithm for working with files

--- file_parser.py
+++ file_parser.py
@@ -5,34 +5,55 @@ import codecs
 class FileParser:
     def __init__(self, filename):
         self.__filename = filename
-        self.__data = {}
-        self.__read = []
-        self.__tile_num = str()
+
+        # to return
+        self.__tiles = []
+        self.__max_read = 0
+        self.__coefficient_per_tile = {}
+
+        # for get data from file
+        self.__cur_tile = str()
+        self.__num_of_n = []
+        self.__num_of_read = 0
+
+    def createFiles(self, useful, useless, tiles_pool):
+        print('Creating files....')
+        if '.gz' in self.__filename:
+            with gzip.open(self.__filename, 'rb') as default_file, \
+                    gzip.open(useless, 'wb') as useless_file, \
+                    gzip.open(useful, 'wb') as useful_file:
+                for pos, line in enumerate(default_file):
+                    line = codecs.decode(line, 'UTF-8')
+                    self.__write(pos, line, useless_file, useful_file,
+                                 tiles_pool)
+        else:
+            with open(self.__filename, 'r') as default_file, \
+                    gzip.open(useless, 'wb') as useless_file, \
+                    gzip.open(useful, 'wb') as useful_file:
+                for pos, line in enumerate(default_file):
+                    self.__write(pos, line, useless_file, useful_file,
+                                 tiles_pool)
+
+    def __write(self, pos, line, useless_file, useful_file, tiles_pool):
+        if pos % 4 == 0:
+            self.__cur_tile = line.split(':')[2]
+
+        if self.__cur_tile not in self.__tiles:
+            print(self.__cur_tile)
+            self.__tiles.append(self.__cur_tile)
+
+        if self.__cur_tile in tiles_pool:
+            useless_file.write(codecs.encode(line, 'UTF-8'))
+        else:
+            useful_file.write(codecs.encode(line, 'UTF-8'))
 
     def parse(self):
+        print('Reading file...')
         if '.gz' in self.__filename:
             self.__gz_parse()
         else:
             self.__txt_parse()
-        return self.__data
-
-    def createFile(self, data):
-        with gzip.open(self.__filename, 'wb') as file:
-            for tile in data.values():
-                for read in tile:
-                    for line in read:
-                        file.write(codecs.encode(line, 'UTF-8'))
-
-    def __file_iteration(self, pos, line):
-        if pos % 4 == 0:
-            self.__tile_num = line.split(':')[2]
-        self.__read.append(line)
-
-        if pos % 4 == 3:
-            if self.__tile_num not in self.__data:
-                self.__data[self.__tile_num] = []
-            self.__data[self.__tile_num].append(self.__read.copy())
-            self.__read = []
+        return self.__tiles, self.__max_read, self.__coefficient_per_tile
 
     def __txt_parse(self):
         with open(self.__filename, 'r') as file:
@@ -44,3 +65,26 @@ class FileParser:
             for pos, line in enumerate(file):
                 line = codecs.decode(line, 'UTF-8')
                 self.__file_iteration(pos, line)
+
+    def __file_iteration(self, pos, line):
+        if pos % 4 == 0:
+            self.__cur_tile = line.split(':')[2]
+
+        if self.__cur_tile not in self.__tiles:
+            print(self.__cur_tile)
+            self.__num_of_read = 0
+            self.__tiles.append(self.__cur_tile)
+
+        if pos % 4 == 1:
+            str_len = len(line)
+            self.__max_read = max(self.__max_read, str_len)
+            if self.__num_of_read == 0:  # init self.__num_of_n
+                self.__num_of_n = [0] * self.__max_read
+                self.__coefficient_per_tile[self.__cur_tile] = \
+                    [0] * self.__max_read
+            self.__num_of_read += 1
+            for i in range(str_len):
+                if line[i].lower() == 'n':
+                    self.__num_of_n[i] += 1
+                self.__coefficient_per_tile[self.__cur_tile][i] = \
+                    self.__num_of_n[i] / self.__num_of_read
--- main_window.py
+++ main_window.py
@@ -8,12 +8,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.all_data = {}
-        # {'tile'': [['id', 'нуклеотид', 'id', 'quality'],[...]]}
-        self.useless_data = {}
-        # тоже что и all_data, но то что будет выброшено при чистке
+        self.tiles = []
+        self.coefficient_per_tile = {}
+        self.max_read = 0
+        self.file_path = str()
+
         self.tiles_pool = []
-        # список тайлов которые необходимо удалить
 
         self.initUI()
 
@@ -25,46 +25,42 @@ class MainWindow(QMainWindow):
         openFile.triggered.connect(self.parseFileToData)
         self.deleteTilesButton.clicked.connect(self.deleteTiles)
         self.acceptAddTileButton.clicked.connect(self.addUselessTile)
-        self.exportToFilesButton.clicked.connect(self.exportToFiles)
+        # self.exportToFilesButton.clicked.connect(self.exportToFiles)
         self.clearUselessTilesButton.clicked.connect(self.clearUselessTiles)
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key.Key_Delete:
             self.deleteUselessTile()
 
-    # удаляет из списка тайлов, которые необходимо удалить, выделенный тайл
     def deleteUselessTile(self):
         items = self.uselessTileList.selectedItems()
         for i in items:
             self.tiles_pool.remove(i.text())
         self.createUselessTileList()
 
-    # очищает списко тайлов, которые необходимо удалить
     def clearUselessTiles(self):
         self.uselessTileList.clear()
         self.tiles_pool = []
 
-    # добавляет в список тайл, который необходимо удалить, из text edit
     def addUselessTile(self):
         tile = self.addTileTextEdit.toPlainText()
-        if tile in self.tiles_pool or tile not in self.all_data:
+        if tile in self.tiles_pool or tile not in self.tiles:
             return
         self.addTileTextEdit.clear()
         self.tiles_pool.append(tile)
         self.createUselessTileList()
 
-    # создает список тайлов, которые необходимо удалить, из self.tiles_pool
     def createUselessTileList(self):
         self.uselessTileList.clear()
         for pos, i in enumerate(self.tiles_pool):
             self.uselessTileList.addItem(i)
 
-    # создает окно выбора файла и парсит файл в self.all_data
     def parseFileToData(self):
-        file_name = QFileDialog.getOpenFileName(self, 'Open File With Data')[0]
-        if file_name == '':
+        self.file_path = QFileDialog.getOpenFileName(self, 'Open File With Data')[0]
+        if self.file_path == '':
             return
-        self.all_data = FileParser(file_name).parse()
+        self.tiles, self.max_read, self.coefficient_per_tile = FileParser(
+            self.file_path).parse()
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
         msg.setText('Done')
@@ -72,38 +68,35 @@ class MainWindow(QMainWindow):
         msg.exec_()
         self.createAllTileList()
 
-    # создает список тайлов, которые присутствуют в файле
     def createAllTileList(self):
         self.allTileList.clear()
-        for pos, i in enumerate(self.all_data):
+        for pos, i in enumerate(self.tiles):
             self.allTileList.addItem(i)
 
-    # удаляет лишние тайлы из self.all_data и записывает их в self.useless_data
     def deleteTiles(self):
-        for pos, i in enumerate(self.tiles_pool):
-            self.useless_data[i] = self.all_data[i]
-            self.all_data.pop(i)
-        self.tiles_pool = []
-        self.createAllTileList()
-        self.createUselessTileList()
-
-    # экспортит данные в два файла: с полезными и лишними данными
-    def exportToFiles(self):
-        if not self.useless_data:
-            return
         name_useless = QFileDialog.getSaveFileName(self, 'Save Useless Data')[0]
-        name_all = QFileDialog.getSaveFileName(self, 'Save Useful Data')[0]
-        if name_all == '':
-            name_all = 'useful.txt.gz'
         if name_useless == '':
             name_useless = 'useless_sequence.txt.gz'
+        name_all = QFileDialog.getSaveFileName(self, 'Save Useful Data')[0]
+        if name_all == '':
+            name_all = 'useful_sequence.txt.gz'
+
         name_all = ''.join(name_all.split('.')) + '.txt.gz'
         name_useless = ''.join(name_useless.split('.')) + '.txt.gz'
-        FileParser(name_useless).createFile(self.useless_data)
-        FileParser(name_all).createFile(self.all_data)
-        self.useless_data = {}
+
+        FileParser(self.file_path).createFiles(name_all, name_useless,
+                                               self.tiles_pool)
+
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
         msg.setText('Done')
         msg.setStandardButtons(QMessageBox.Ok)
         msg.exec_()
+
+        for i in self.tiles:
+            if i in self.tiles_pool:
+                self.tiles.remove(i)
+        self.tiles_pool = []
+
+        self.createAllTileList()
+        self.createUselessTileList()
